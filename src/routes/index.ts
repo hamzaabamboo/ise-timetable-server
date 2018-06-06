5f7a69f503d7c60d46f29ac0ce8aa53f70461bcd
Tanyawat <hamzaabamboo@gmail.com>
2018-06-06 14:35:10 +0700

    Fix gitignore once again and routing problem

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { Router } from "express";
-import {router as subjectRouter } from './subjects';
+import subjectRouter from "./subjects";
 const router = Router();
 /* GET home page. */
 router.get("/", (req, res) => {
-  console.log('helloworld');
-  res.status(200).send("Hello World");
+  res.status(200).send("Hello World 2");
 });
-router.use('/subjects',subjectRouter);
+
+router.use("/subjects", subjectRouter);
 export default router;
--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import { Router } from "express";
-import { SubjectModel } from "../../models/subject.models";
-export const router = Router();
+import { SubjectModel } from "../models/subject.models";
+const router = Router();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/:id', async (req,res,next) => {
-	const {id} = req.params;
-	console.log(id);
-	const subject = await SubjectModel.findOne({id: id});
-	res.json(subject);
-})
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  const subject = await SubjectModel.findOne({ id: id });
+  res.json(subject);
+});
 
+export default router;

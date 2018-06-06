d7e151d6f332e25816faefbe5276abd68bc9d40b
him k <him10424@gmail.com>
2018-06-06 13:37:24 +0700

    add routes/models

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { Router } from "express";
+import {router as subjectRouter } from './subjects';
 const router = Router();
 /* GET home page. */
 router.get("/", (req, res) => {
+  console.log('helloworld');
   res.status(200).send("Hello World");
 });
-
+router.use('/subjects',subjectRouter);
 export default router;
